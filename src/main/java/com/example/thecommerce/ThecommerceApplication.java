1d80514fbd1a8ee560aa36e4fad3c465c7fa0a90
LeeSeokWoon <leeseokwoon@naver.com>
2024-04-17 14:24:36 +0900

    bug:MainApplication 누락소스추가

---
+++ src/main/java/com/example/thecommerce/ThecommerceApplication.java
@@ -0,0 +1,12 @@
+package com.example.thecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThecommerceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ThecommerceApplication.class, args);
+	}
+}
+
