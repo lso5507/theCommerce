e4404ae4c6e1d959c83166b689222cf411e675fc
lso5507 <49707719+lso5507@users.noreply.github.com>
2024-04-21 14:31:11 +0900

    refactor:에러핸들링소스수정

--- src/main/java/com/example/thecommerce/user/handler/CustomExceptionHandler.java
+++ src/main/java/com/example/thecommerce/user/handler/CustomExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.example.thecommerce.user.handler;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,17 +12,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.thecommerce.user.dto.ErrorResponse;
+
 @RestControllerAdvice
 public class CustomExceptionHandler {
 	Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
 	@ExceptionHandler(SQLException.class)
 	public ResponseEntity<?> handleSQLException(SQLException ex) {
 		logger.error("SQLException::", ex);
-		return ResponseEntity.internalServerError().build();
+		ErrorResponse errorResponse= new ErrorResponse(Collections.singletonList("SQLException"), HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ResponseEntity.internalServerError().body(errorResponse);
 	}
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<?> handleIllegalArgumentException(IllegalArgumentException ex){
 		logger.error("IllegalArgumentException::", ex);
-		return ResponseEntity.internalServerError().build();
+		ErrorResponse errorResponse= new ErrorResponse(Collections.singletonList("IllegalArgumentException"), HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ResponseEntity.internalServerError().body(errorResponse);
+	}
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<?> handleException(IllegalArgumentException ex){
+		logger.error("handleException::", ex);
+		ErrorResponse errorResponse= new ErrorResponse(Collections.singletonList("handleException"), HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ResponseEntity.internalServerError().body(errorResponse);
 	}
 }
